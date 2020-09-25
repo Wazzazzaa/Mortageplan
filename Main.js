d1be43f8dc66081a975e86db06bccd6f93b23d10
wazzazzaa <tomi.eranne@gmail.com>
2020-09-25 10:15:37 +0300

    database

---
+++ Main.js
@@ -0,0 +1,32 @@
+const mysql = require('mysql');
+let http = require('http');
+
+//DB connection
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'perunamuussi',
+  database : 'mortageplan',
+  port     : 3306
+});
+connection.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Connected to database');
+});
+//Get customers info from DB
+  connection.query("SELECT * FROM customers", function(err, result) {
+    if (err) throw err;
+    let jsonresult = JSON.stringify(result);
+    let obj = JSON.parse(jsonresult);
+    let arrayl = result.length;
+    for (let i = 0; i < arrayl; i++) {
+      let A = obj[i].customer;
+          let x = obj[i].loan;
+          let y = obj[i].interest / 100 /12;
+          let z = obj[i].years * 12;
+      let E = x * (y* (1 + y) ** (z)) / (((1 + y ) ** (z ) - 1));
+console.log(A+ " " + E.toFixed(2));
+    }
+  });
